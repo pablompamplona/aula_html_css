8c9d7ef91e4eb04fa44dab3ce00809506bd1652d
Pablo Alexandre Pamplona <pablompamplona@gmail.com>
2022-07-21 23:46:23 -0300

    ajustada tabela imc

--- JavaScript/calculadoraImc.js
+++ JavaScript/calculadoraImc.js
@@ -6,21 +6,21 @@ function calcular() {
 
     let imc = peso.value / (altura.value * altura.value);
     if (imc < 18.5) {
-        result.innerHTML = 'Magreza  -  Obesidade: 0';
-        result.style.color = 'yelow';
+        result.innerHTML = 'Abaixo do Peso';
+        result.style.color = 'yellow';
         result.style.fontWeight = 'bold';
     }
     else if (imc >= 18.5 && imc < 25) {
-        result.innerHTML = 'Normal  -  Obesidade: 0';
+        result.innerHTML = 'Peso Normal';
         result.style.color = 'green';
         result.style.fontWeight = 'bold';
     }
-    else if (imc <= 25 && imc < 30) {
+    else if (imc >= 25 && imc < 30) {
         result.innerHTML = 'Sobrepeso  -  Obesidade: I';
-        result.style.color = 'yelow';
+        result.style.color = 'yellow';
         result.style.fontWeight = 'bold';
     }
-    else if (imc <= 30 && imc < 40) {
+    else if (imc >= 30 && imc < 40) {
         result.innerHTML = 'Obesidade  -  Obesidade: II';
         result.style.color = 'red';
         result.style.fontWeight = 'bold';
